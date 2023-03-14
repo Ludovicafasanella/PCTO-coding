5c04b9079cf86a95693656e1f9916d973095ce09
Ludovicafasanella <127746586+Ludovicafasanella@users.noreply.github.com>
2023-03-14 11:21:11 +0100

    Add files via upload

---
+++ ehi.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main ()
+{
+    int eta;
+    printf ("qual è la tua eta?");
+    scanf("%d", &eta );
+    printf ( "la mia eta è %d\n", eta);
+
+if ( eta >18)
+{
+    printf ( "la condizione è verificata\n");
+}
+else
+{
+    printf ( "la condizione è verificata\n");
+}
+}
