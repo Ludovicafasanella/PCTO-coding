49511df3a2f422fa55c1184180206986b96563b9
Ludovicafasanella <127746586+Ludovicafasanella@users.noreply.github.com>
2023-03-14 14:23:30 +0100

    Add files via upload

---
+++ numero.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()  
+{
+int anno;
+printf( "quale anno vuoi analizzare?\n");
+scanf ("%d", &anno);
+
+if ((anno % 4== 0 && anno %100 !=0 )|| anno %400==0)
+{
+    printf( "l'anno è bisestile!\n");
+}
+
+else
+{
+    printf("l'anno non è bisestile\n");
+}
+
+
+}
